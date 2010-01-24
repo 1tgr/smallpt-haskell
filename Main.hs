9aa6eace83b863a2d82209b1da8e834095f808cd
Tim Robinson <tim.g.robinson@gmail.com>
2010-01-24 14:11:42 +0000

    Something is wrong with diffuse reflection

--- Main.hs
+++ Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE PatternGuards, ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
 import Control.Applicative
@@ -50,11 +50,11 @@ data Sphere a = Sphere { radius :: a,
                          colour :: Vec a,
                          refl :: Refl }
 
-intersectSphere :: (Floating a, Ord a) => Sphere a -> Ray a -> Maybe a
-intersectSphere s (Ray o d) = let op = (position s) |-| o -- Solve t^2*d.d + 2*t*(o-p).d + (o-p).(o-p)-R^2 = 0
+intersectSphere :: (Floating a, Ord a) => Ray a -> Sphere a -> Maybe a
+intersectSphere (Ray o d) s = let op = position s |-| o -- Solve t^2*d.d + 2*t*(o-p).d + (o-p).(o-p)-R^2 = 0
                                   eps = 1e-4
                                   b = op `dot` d
-                                  det = (b * b) - (op `dot` op) + ((radius s) * (radius s))
+                                  det = (b * b) - (op `dot` op) + (radius s * radius s)
                               in if det < 0
                                    then Nothing
                                    else let det' = sqrt det
@@ -67,14 +67,14 @@ intersectSphere s (Ray o d) = let op = (position s) |-| o -- Solve t^2*d.d + 2*t
                                                        else Nothing
 
 spheres :: Fractional a => [Sphere a]
-spheres = [Sphere { radius = 1e5,  position = Vec (1+1e5) 40.8 81.6,    emission = Vec 0 0 0,    colour = Vec 0.75 0.25 0.25,  refl = DIFF},--Left
+spheres = [{- Sphere { radius = 1e5,  position = Vec (1+1e5) 40.8 81.6,    emission = Vec 0 0 0,    colour = Vec 0.75 0.25 0.25,  refl = DIFF},--Left
            Sphere { radius = 1e5,  position = Vec (99-1e5) 40.8 81.6,   emission = Vec 0 0 0,    colour = Vec 0.25 0.25 0.75,  refl = DIFF},--Rght
            Sphere { radius = 1e5,  position = Vec 50 40.8 1e5,          emission = Vec 0 0 0,    colour = Vec 0.75 0.75 0.75,  refl = DIFF},--Back
            Sphere { radius = 1e5,  position = Vec 50 40.8 (170-1e5),    emission = Vec 0 0 0,    colour = Vec 0 0 0,           refl = DIFF},--Frnt
            Sphere { radius = 1e5,  position = Vec 50 1e5 81.6,          emission = Vec 0 0 0,    colour = Vec 0.75 0.75 0.75,  refl = DIFF},--Botm
-           Sphere { radius = 1e5,  position = Vec 50 (81.6-1e5) 81.6,   emission = Vec 0 0 0,    colour = Vec 0.75 0.75 0.75,  refl = DIFF},--Top
-           Sphere { radius = 16.5, position = Vec 27 16.5 47,           emission = Vec 0 0 0,    colour = (Vec 1 1 1)|*|0.999, refl = SPEC},--Mirr
-           Sphere { radius = 16.5, position = Vec 73 16.5 78,           emission = Vec 0 0 0,    colour = (Vec 1 1 1)|*|0.999, refl = REFR},--Glas
+           Sphere { radius = 1e5,  position = Vec 50 (81.6-1e5) 81.6,   emission = Vec 0 0 0,    colour = Vec 0.75 0.75 0.75,  refl = DIFF},--Top -}
+           Sphere { radius = 16.5, position = Vec 27 16.5 47,           emission = Vec 0 0 0,    colour = Vec 1 1 1 |*| 0.999, refl = SPEC},--Mirr
+           Sphere { radius = 16.5, position = Vec 73 16.5 78,           emission = Vec 0 0 0,    colour = Vec 1 1 1 |*| 0.999, refl = REFR},--Glas
            Sphere { radius = 600,  position = Vec 50 (681.6-0.27) 81.6, emission = Vec 12 12 12, colour = Vec 0 0 0,           refl = DIFF}] --Lite
 
 clamp :: (Num a, Ord a) => a -> a
@@ -83,15 +83,14 @@ clamp x | x < 0 = 0
         | otherwise = x
 
 toInt :: (Floating a, Ord a, RealFrac a, Integral b) => a -> b
-toInt x = truncate ((((clamp x) ** (1 / 2.2)) * 255) + 0.5)
+toInt x = truncate (((clamp x ** (1 / 2.2)) * 255) + 0.5)
 
 maybeMinimumBy :: (a -> a -> Ordering) -> [a] -> Maybe a
-maybeMinimumBy f [] = Nothing
+maybeMinimumBy _ [] = Nothing
 maybeMinimumBy f l = Just (minimumBy f l)
 
 intersectScene :: (Floating a, Ord a) => Ray a -> Maybe (Sphere a, a)
-intersectScene r = maybeMinimumBy (comparing snd)
-                 $ [(s, t) | (s, Just t) <- [(s, intersectSphere s r) | s <- spheres]]
+intersectScene r = maybeMinimumBy (comparing snd) [(s, t) | (s, Just t) <- map ((,) <*> intersectSphere r) spheres]
 
 radiance :: (Floating a, Ord a, Random a, RandomGen g) => Ray a -> Int -> State g (Vec a)
 radiance r depth = case intersectScene r of
@@ -99,7 +98,7 @@ radiance r depth = case intersectScene r of
                      Just (obj, t) -> radiance' obj t
   where radiance' obj t = do let Ray raypos raydir = r
                                  x = raypos |+| (raydir |*| t)
-                                 n = norm (x |-| (position obj))
+                                 n = norm (x |-| position obj)
                                  nl | (n `dot` raydir) < 0 = n
                                     | otherwise = n |*| (-1)
                                  Vec fx fy fz = colour obj
@@ -111,19 +110,20 @@ radiance r depth = case intersectScene r of
                                      else State random
                              if p' >= p
                                then return (emission obj) --R.R.
-                               else let f = (colour obj) |*| (1.0 / p)
+                               else let f = colour obj |*| (1.0 / p)
                                     in case refl obj of 
                                       DIFF -> -- Ideal DIFFUSE reflection
                                               do r1 <- (2 * pi *) <$> State random
                                                  r2 <- State random
                                                  let r2s = sqrt r2
-                                                     Vec wx _ _ = nl
-                                                     u | (abs wx) > 0.1 = norm ((Vec 0 1 0) `cross` nl)
-                                                       | otherwise = norm ((Vec 1 0 0) `cross` nl)
-                                                     v = nl `cross` u
-                                                     d = norm ((u |*| ((cos r1) * r2s)) |+| 
-                                                               (v |*| ((sin r1) * r2s)) |+| 
-                                                               (nl |*| (sqrt (1 - r2))))
+                                                     w = nl
+                                                     Vec wx _ _ = w
+                                                     u | abs wx > 0.1 = norm (Vec 0 1 0 `cross` w)
+                                                       | otherwise = norm (Vec 1 0 0 `cross` w)
+                                                     v = w `cross` u
+                                                     d = norm ((u |*| (cos r1 * r2s)) |+| 
+                                                               (v |*| (sin r1 * r2s)) |+| 
+                                                               (w |*| sqrt (1 - r2)))
                                                  ((emission obj) |+|) . (f `vmult`) <$> radiance (Ray x d) (depth + 1)
                                       SPEC -> -- Ideal SPECULAR reflection
                                               ((emission obj) |+|) . (f `vmult`) <$> radiance (Ray x (raydir |-| (n |*| (2 * (n `dot` raydir))))) (depth + 1)
@@ -138,7 +138,7 @@ radiance r depth = case intersectScene r of
                                                   cos2t = 1 - (nnt * nnt * (1 - (ddn * ddn)))
                                               in if cos2t < 0    -- Total internal reflection
                                                    then ((emission obj) |+|) . (f `vmult`) <$> radiance reflRay (depth + 1)
-                                                   else let tdir = norm ((raydir |*| nnt) |-| (n |*| ((if into then 1 else (-1)) * (ddn * nnt + (sqrt cos2t)))))
+                                                   else let tdir = norm ((raydir |*| nnt) |-| (n |*| ((if into then 1 else (-1)) * (ddn * nnt + sqrt cos2t))))
                                                             a = nt - nc
                                                             b = nt + nc
                                                             r0 = a * a / (b * b)
@@ -157,37 +157,42 @@ radiance r depth = case intersectScene r of
                                                                                 return (re' |+| tr')
                                                         in ((emission obj) |+|) . (f `vmult`) <$> r'
 
+data ConstGen = ConstGen
+
+instance RandomGen ConstGen where
+  genRange = const (0, 9)
+  next = (,) 4
+  split = join (,)
+
 main' :: forall a. (Enum a, Floating a, NFData a, Ord a, Random a) => Int -> Int -> Int -> [[Vec a]]
 main' w h samp = parMap rdeepseq (line . (h -)) [1..h]
-                 where one_over_w = 1.0 / (fromIntegral w) :: a
-                       one_over_h = 1.0 / (fromIntegral h) :: a
-                       one_over_samp = 1.0 / (fromIntegral samp) ::a
+                 where one_over_w = 1.0 / fromIntegral w :: a
+                       one_over_h = 1.0 / fromIntegral h :: a
+                       one_over_samp = 1.0 / fromIntegral samp ::a
                        campos = Vec 50 52 295.6
                        camdir = norm (Vec 0 (-0.042612) (-1))
                        cx = Vec (0.5135 * one_over_h / one_over_w) 0 0
-                       cy = (norm (cx `cross` camdir)) |*| 0.5135
-                       line y = let m = sequence (map ((flip pixel y) . subtract 1) [1..w])
+                       cy = norm (cx `cross` camdir) |*| 0.5135
+                       line y = let m = mapM (flip pixel y . subtract 1) [1..w]
                                     g = mkStdGen (y * y * y)
-                                in evalState (trace ("Line " ++ (show y)) m) g
-                       pixel x y = foldl1 (|+|) <$> sequence [subpixel x y sx sy | sy <- [0, 1], sx <- [0, 1]]
-                       subpixel x y sx sy = do Vec rx ry rz <- foldl1 (|+|) <$> sequence (replicate samp (sample x y sx sy))
-                                               return (Vec (clamp rx) (clamp ry) (clamp rz) |*| 0.25)
+                                in evalState (trace ("Line " ++ show y) m) g
+                       pixel x y = (|*| 0.25) . foldl1 (|+|) <$> sequence [subpixel x y sx sy | sy <- [0 :: Int, 1], sx <- [0 :: Int, 1]]
+                       subpixel x y sx sy = do Vec rx ry rz <- (|*| one_over_samp) . foldl1 (|+|) <$> replicateM samp (sample x y sx sy)
+                                               return (Vec (clamp rx) (clamp ry) (clamp rz))
                        sample x y sx sy = do r1 <- (2 *) <$> State random
                                              r2 <- (2 *) <$> State random
-                                             let dx | r1 < 1 = (sqrt r1) - 1
-                                                    | otherwise = 1 - (sqrt (2 - r1))
-                                                 dy | r2 < 1 = (sqrt r2) - 1
-                                                    | otherwise = 1 - (sqrt (2 - r2))
-                                                 floatx = fromIntegral x :: a
-                                                 floaty = fromIntegral y :: a
-                                                 d = (cx |*| (((sx + 0.5 + dx) / 2 + floatx) * one_over_w - 0.5)) |+|
-                                                     (cy |*| (((sy + 0.5 + dy) / 2 + floaty) * one_over_h - 0.5)) |+| camdir
+                                             let dx | r1 < 1 = sqrt r1 - 1
+                                                    | otherwise = 1 - sqrt (2 - r1)
+                                                 dy | r2 < 1 = sqrt r2 - 1
+                                                    | otherwise = 1 - sqrt (2 - r2)
+                                                 d = (cx |*| (((((fromIntegral sx :: a) + 0.5 + dx) / 2 + (fromIntegral x :: a)) * one_over_w) - 0.5)) |+|
+                                                     (cy |*| (((((fromIntegral sy :: a) + 0.5 + dy) / 2 + (fromIntegral y :: a)) * one_over_h) - 0.5)) |+| camdir
                                                  ray = Ray (campos |+| (d |*| 140.0)) (norm d)
-                                             (|*| one_over_samp) <$> radiance ray 0
+                                             radiance ray 0
 
 main :: IO ()
 main = do let w = 1024
               h = 768
-              showPixel (Vec r g b) = (show (toInt r :: Int)) ++ " " ++ (show (toInt g :: Int)) ++ " " ++ (show (toInt b :: Int)) ++ " "
-          putStrLn ("P3\n" ++ (show w) ++ " " ++ (show h) ++ "\n255\n")
-          mapM_ (putStrLn . showPixel) (concat (main' w h 1) :: [Vec Float])
+              showPixel (Vec r g b) = show (toInt r :: Int) ++ " " ++ show (toInt g :: Int) ++ " " ++ show (toInt b :: Int) ++ " "
+          putStrLn ("P3\n" ++ show w ++ " " ++ show h ++ "\n255\n")
+          mapM_ (putStrLn . showPixel) (concat (main' w h 100) :: [Vec Float])
--- Makefile
+++ Makefile
@@ -2,10 +2,13 @@ all: test
 
 build:
 	mkdir -p bin obj
+	hlint *.hs
 	ghc --make -Wall -threaded -o bin/global-illum -outputdir obj *.hs
 
 clean:
 	rm -r bin obj
 
-test: build
+bin/global-illum.ppm: bin/global-illum
 	time bin/global-illum +RTS -N8 > bin/global-illum.ppm
+
+test: build bin/global-illum.ppm
