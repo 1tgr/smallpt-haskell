0284fdf00678c574a879a46610371e4bb74deb6f
Tim Robinson <tim.g.robinson@gmail.com>
2010-02-27 12:04:24 +0000

    Smarter render script

--- ec2/ec2-render.sh
+++ ec2/ec2-render.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-twidge update "`date +%T` Render is starting"
 smallpt=dist/build/smallpt/smallpt
-workers="\"$smallpt --runWorker\""
-if $smallpt -samples 8000 $workers; then
+
+function makeWorkers {
+  yes "\"$smallpt -r\"" | head -n 8
+  aws describe-instances --simple | awk '$2 == "running" { printf "\"ssh root@%s /root/.cabal/bin/smallpt -r\"\n", $3 }'
+}
+
+twidge update "`date +%T` Render is starting"
+if makeWorkers | xargs $smallpt --samples 8000
+then
   s3put --public partario/public/image.png image.png
   twidge update "`date +%T` Render is finished - http://s3.amazonaws.com/partario/public/image.png"
 else
