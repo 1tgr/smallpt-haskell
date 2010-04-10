d8c10d253ff79e314d5afba8a67596b8ab448f8f
Tim Robinson <tim.g.robinson@gmail.com>
2010-04-10 17:40:23 +0100

    Rename variables

--- Tim/Smallpt/Chart.hs
+++ Tim/Smallpt/Chart.hs
@@ -13,21 +13,22 @@ data Chart a = Chart { chartEnter :: a -> IO (IO ()),
                        chartSave :: IO () }
 
 makeChart :: forall a. (Ord a, Show a) => IO (Chart a)
-makeChart = do rowsMVar <- newMVar (M.empty :: M.Map a (Double, [Html]))
+makeChart = do barsMVar <- newMVar (M.empty :: M.Map a (Double, [Html]))
                startTime <- getCurrentTime
                let getCurrentSecs = fromRational . toRational . flip diffUTCTime startTime <$> getCurrentTime
                    leave n t1 = do t2 <- getCurrentSecs
-                                   rows <- takeMVar rowsMVar
-                                   let (right, cells) = M.findWithDefault (0, []) n rows
-                                       cell 0 _ = Nothing
-                                       cell width colour = Just ((thediv ! [thestyle (printf "float: left; width: %dpx; background-color: %s" (width :: Int) (colour :: String))]) (primHtml "&nbsp;"))
-                                       (cell1, cell2) | t1 - right < 1 = (cell 1 "#fff", cell (truncate (t2 - t1) - 1) "#000")
-                                                      | otherwise = (cell (truncate (t1 - right)) "#fff", cell (truncate (t2 - t1)) "#000")
-                                   putMVar rowsMVar (M.insert n (t2, cells ++ catMaybes [cell1, cell2]) rows)
+                                   bars <- takeMVar barsMVar
+                                   let (right, cells) = M.findWithDefault (0, []) n bars
+                                       bar 0 _ = Nothing
+                                       bar px cssClass = Just (thediv ! [thestyle (printf "width: %dpx" (px :: Int)), theclass cssClass] << primHtml "&nbsp;")
+                                       barCssClass = "bar-" ++ show n
+                                       maybeCells | t1 - right < 1 = [bar 1 "blank", bar (truncate (t2 - t1) - 1) barCssClass]
+                                                  | otherwise = [bar (truncate (t1 - right)) "blank", bar (truncate (t2 - t1)) barCssClass]
+                                   putMVar barsMVar (M.insert n (t2, cells ++ catMaybes maybeCells) bars)
                    enter n = do t1 <- getCurrentSecs
                                 return (leave n t1)
-                   save = do let makeRow n (_, cells) = tr (th (stringToHtml (show n)) +++ td (concatHtml cells))
-                             trs <- concatHtml . map (uncurry makeRow) . M.assocs <$> readMVar rowsMVar
-                             writeFile "work.html" (renderHtml (thehtml (body (table trs))))
+                   save = do let makeRow n (_, cells) = tr << (th << show n +++ td << concatHtml cells)
+                             trs <- concatHtml . map (uncurry makeRow) . M.assocs <$> readMVar barsMVar
+                             writeFile "work.html" (renderHtml (thehtml << (body << table trs)))
                return Chart { chartEnter = enter,
                               chartSave = save }
